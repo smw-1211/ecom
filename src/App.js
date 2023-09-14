06e9215471d43f5a65e4fc705539c96dd4d2054d
Soham Wattamwar <Soham.Wattamwar@blueconchtech.com>
2023-09-14 13:12:13 +0530

    created static navbar page and added routing

--- src/App.js
+++ src/App.js
@@ -4,18 +4,22 @@ import { Routes, Route } from "react-router-dom"
 import { Products } from './pages/products/index'
 import { Product } from './pages/product/index'
 import { NotFound } from './pages/not found/index'
+import { NavBar } from './components/navbar/index'
 import { Cart } from './pages/cart/index'
 
 function App() {
   return (
     <div className="App">
-
       Hello World
-
-      <Products />
-      <Product />
-      <NotFound />
-      <Cart />
+      <>
+        <NavBar />
+        <Routes>
+          <Route path='/' element={<Products />} />
+          <Route path='/product/:productId' element={<Product />} />
+          <Route path='/cart' element={<Cart />} />
+          <Route path='*' element={<NotFound />} />
+        </Routes>
+      </>
     </div>
   );
 }
---
+++ src/components/navbar/navbar.js
@@ -0,0 +1,32 @@
+import { Link } from "react-router-dom"
+
+const NavBar = () => {
+    return (
+        <div className="wrapper">
+            <header className="container">
+                <div className="header py-2">
+                <div className="grid">
+                    <Link to="/" className="link">
+                        <h1 className="brand">E-commerce</h1>
+                    </Link>
+                    <div className="formContainer">
+                        <form className="search">
+                            <div className="fomr-control">
+                                <input type="text" placeholder="Search products.." />
+                            </div>
+                            <button type="button" className="search-btn">
+                                Search
+                            </button>
+                        </form>
+                    </div>
+                    <Link to="/cart" className="link-headerCart">
+                        <img className="cartImg" src="/cart.svg" alt="cart"/>
+                    </Link>
+                    </div>
+                </div>
+            </header>
+        </div>
+    )
+}
+
+export { NavBar }
