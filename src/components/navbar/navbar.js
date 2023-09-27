9b9c4d9e165620775ccfb6bf57d715f4c7c9af87
smw-1211 <wattamwarsoham2000@gmail.com>
2023-09-28 00:17:27 +0530

    Updated not found page preview and made some changes in navbar component

--- src/components/navbar/navbar.js
+++ src/components/navbar/navbar.js
@@ -6,35 +6,41 @@ const NavBar = ({ onSearch, cartItemCount }) => {
     const [searchQuery, setSearchQuery] = useState('');
 
     const handleSubmit = () => {
-        if(searchQuery.trim().length) {
-            onSearch(searchQuery.trim());
+        if (searchQuery.trim().length) {
+            onSearch(searchQuery.trim())
         }
         setSearchQuery('')
     }
+
     return (
         <div className="wrapper">
             <header className="container">
                 <div className="header py-2">
-                <div className="grid">
-                    <Link to="/" className="link">
-                        <h1 className="brand">E-commerce</h1>
-                    </Link>
-                    <div className="formContainer">
-                        <form className="search">
-                            <div className="fomr-control">
-                                <input value={searchQuery} onChange={e => setSearchQuery(e.target.value)} type="text" placeholder="Search products.." />
-                            </div>
-                            <button type="button" className="search-btn" onClick={handleSubmit}>
-                                Search Product
-                            </button>
-                        </form>
-                    </div>
-                    <Link to="/cart" className="link-headerCart">
-                        <img className="cartImg" src="/cart.svg" alt="cart"/>
-                        {cartItemCount > 0 && (
-                            <div className="cartCounter">{cartItemCount <= 9 ? cartItemCount : "9+"}</div>
-                        )}
-                    </Link>
+                    <div className="grid">
+                        <Link to="/" className="link">
+                            <h1 className="brand">E-commerce</h1>
+                        </Link>
+                        <div className="formContainer">
+                            <form className="search">
+                                <div className="form-control">
+                                    <input
+                                        type="text"
+                                        value={searchQuery}
+                                        onChange={e => setSearchQuery(e.target.value)}
+                                        placeholder="Search products..."
+                                    />
+                                </div>
+                                <button type="button" className="search-btn" onClick={handleSubmit} >
+                                    Search
+                                </button>
+                            </form>
+                        </div>
+                        <Link to="/cart" className="link headerCart">
+                            <img className="cartImg" src="/cart.svg" alt="cart" />
+                            {cartItemCount > 0 && (
+                                <div className="cartCounter">{cartItemCount <= 9 ? cartItemCount : "9+"}</div>
+                            )}
+                        </Link>
                     </div>
                 </div>
             </header>
--- src/pages/not found/not-found.js
+++ src/pages/not found/not-found.js
@@ -1,10 +1,17 @@
+import { Link } from "react-router-dom"
+
 const NotFound = () => {
     return (
-        <>
-        <div>
-           Not found page goes here.
+        <div className="container">
+            <div className="product py-2">
+                <div className="details p-3">
+                    Page not found. Go to { " " }
+                    <Link to="/" replace>
+                        homepage
+                    </Link>
+                </div>
+            </div>
         </div>
-        </>
     )
 }
 
