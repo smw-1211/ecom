0e7b8aeba32caac9e42706912b988b4fb5fc499b
smw-1211 <wattamwarsoham2000@gmail.com>
2023-09-28 18:10:01 +0530

    Created the cart context

--- src/App.js
+++ src/App.js
@@ -7,16 +7,19 @@ import { NotFound } from './pages/not found/index'
 import { NavBar } from './components/navbar/index'
 import { Cart } from './pages/cart/index'
 
+import { useCart } from './context/cart'
+
 function App() {
 
   const navigate = useNavigate();
+  const { cartItemCount } = useCart();
   const onSearch = (searchQuery) => {
     navigate(`/?${createSearchParams({ q: searchQuery })}`)
   }
   return (
     <div className="App">
       <>
-        <NavBar onSearch={onSearch} cartItemCount={2}/>
+        <NavBar onSearch={onSearch} cartItemCount={cartItemCount()}/>
         <Routes>
           <Route path='/' element={<Products />} />
           <Route path='/product/:productId' element={<Product />} />
